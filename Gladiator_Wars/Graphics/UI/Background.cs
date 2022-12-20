474e5ac747b7b9e109a812389fbfec8b4d0d2e79
Term164 <jani.bangiev@gmail.com>
2022-12-20 22:56:38 +0100

    Implemented Background UI class

---
+++ Gladiator_Wars/Graphics/UI/Background.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator_Wars
+{
+    internal class Background : GraphicsObject
+    {
+        public int size;
+
+        public Background(Vector2 position, int size) : base(position, new Vector2(25*size+7,32), null)
+        {
+            this.size = size;
+            color = Color.SaddleBrown;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(GUIRenderer.backgroundSpriteLeft.texture, position, GUIRenderer.backgroundSpriteLeft.sourceRectangle, color, 0, Vector2.Zero, 4, SpriteEffects.None, 0);
+
+            for(int i = 1; i <= size; i++)
+            {
+                spriteBatch.Draw(GUIRenderer.backgroundSpriteCenter.texture, (position + new Vector2(dimensions.X - 4 - i*25, 0)) * new Vector2(4), GUIRenderer.backgroundSpriteCenter.sourceRectangle,color, 0, Vector2.Zero, 4, SpriteEffects.None, 0);
+            }
+
+            spriteBatch.Draw(GUIRenderer.backgroundSpriteRight.texture, (position + new Vector2(dimensions.X-4,0)) * new Vector2(4), GUIRenderer.backgroundSpriteRight.sourceRectangle, color, 0, Vector2.Zero, 4, SpriteEffects.None, 0);
+        }
+    }
+}
