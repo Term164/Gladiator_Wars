879fd221e051482a1be9603937bd865c364ef51c
Term164 <jani.bangiev@gmail.com>
2022-12-20 22:57:47 +0100

    Extended Text class to make a damageText class that slowly disapears over enemy heads

---
+++ Gladiator_Wars/Graphics/UI/DamageText.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gladiator_Wars
+{
+    internal class DamageText : Text
+    {
+        float opacity;
+        bool isActive;
+
+        public DamageText(Vector2 position, string text, SpriteFont font) : base(position, text, font, Color.Red){
+            opacity = 1f;
+            isActive = false;
+        }
+
+        public override void Update()
+        {
+            if (isActive) {
+                opacity -= 0.01f;
+                color = new Color(255, 0, 0) * opacity;
+                position.Y -= 1.5f;
+                if (opacity <= 0) isActive= false;
+            }
+            else
+            {
+                isActive = false;
+            }
+
+            base.Update();
+        }
+
+        public void resetDamage(Vector2 position, string text)
+        {
+            opacity = 1f;
+            isActive = true;
+            this.position= position;
+            this.text = text;
+        }
+    }
+}
