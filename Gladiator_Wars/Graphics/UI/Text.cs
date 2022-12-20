20f00c2de7ca619f17b40f406bf726beb45164b2
Term164 <jani.bangiev@gmail.com>
2022-12-20 22:57:06 +0100

    Implemented Text UI Element

---
+++ Gladiator_Wars/Graphics/UI/Text.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator_Wars
+{
+    internal class Text : GraphicsObject
+    {
+        public string text;
+        SpriteFont font;
+
+        public Text(Vector2 position, string text, SpriteFont font, Color color) : base(position, Vector2.Zero, null)
+        {
+            this.text = text;
+            this.font = font;
+            this.color = color;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, text, position, color);
+        }
+    }
+}
